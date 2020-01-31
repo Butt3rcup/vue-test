f25d36f1a6be5dfc90a393e4b06df73394c25089
S1mple <S1mple@gmail.com>
2020-01-31 17:35:23 +0800

    loginout

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <h1>Home</h1>
+    <el-button @click="outLogin">
+      退出
+    </el-button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {}
+  },
+  methods: {
+    outLogin () {
+      window.sessionStorage.removeItem('token')
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
-<style lang="less" scoped>
-
-</style>
+<style lang="less" scoped></style>
