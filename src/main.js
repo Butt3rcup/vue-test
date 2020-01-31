23091d5487aacb9150b3c7345ef6c37225bbc73a
S1mple <S1mple@gmail.com>
2020-01-31 17:14:23 +0800

    init Signed-off-by: S1mple <S1mple@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-
+import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
+import Axios from 'axios'
+Vue.prototype.$http = Axios
+Axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.config.productionTip = false
 
 new Vue({
